18cfe92e06aeea9f42a3c8b97936b4a0c35fe33e
Codey <codeyworley@gmail.com>
2018-05-20 14:28:12 -0700

    added client routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,96 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
 
-// Import React Table
-import ReactTable from "react-table";
-import "react-table/react-table.css";
-import "../css/react-table.css"
+// Component Imports
+import Listings from './Listings';
+const Nav = () => <h2>Navbar</h2>
+const Footer = () => <h2>Footer</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const Landing = () => <h2>Landing</h2>
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state ={listings: []};
-  }
-  componentDidMount() {
-    fetch('/api/listings')
-      .then(res => {
-        console.log(res);
-        return res.json()
-      })
-      .then(listings => {
-        console.log(listings);
-        this.setState({ listings })
-      });
-  }
-  // Table Changes Only
-  render() {
-    const { listings } = this.state;
-    return (
-      <div>
-        <ReactTable
-          data={ listings }
-          columns={[
-            {
-              Header: "Info",
-              columns: [
-                {
-                  Header: "Asin",
-                  accessor: "asin",
-                  width: 200
-                },
-                {
-                  Header: "Sku",
-                  accessor: "sku",
-                  width: 250
-                },
-                {
-                  Header: "Title",
-                  accessor: "title"
-                },
-                {
-                  Header: "Price",
-                  accessor: "price",
-                  maxWidth: 100
-                },
-                {
-                  Header: "Inventory",
-                  accessor: "inventory",
-                  maxWidth: 100
-                }
-              ]
-            },
-            {
-              Header: "Status",
-              columns: [
-                {
-                  Header: "Sales Today",
-                  accessor: "sales",
-                  maxWidth: 100
-                },
-                {
-                  Header: "Sales Week",
-                  accessor: "salesweek",
-                  maxWidth: 100
-                },
-                {
-                  Header: "Sales Month",
-                  accessor: "salesmonth",
-                  maxWidth: 100
-                },
-                {
-                  Header: "Days Out of Stock",
-                  accessor: "nostock",
-                  maxWidth: 100
-                }
-              ]
-            }
-          ]}
-          defaultPageSize={25}
-          style={{ }}
-          className="-striped -highlight"
-        />
-        <br />
-      </div>
-    );
-  }
+// App paths
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/dashboard" component={Listings} />
+          <Footer />
+        </div>
+      </BrowserRouter>
+     </div>
+  );
 }
+
 export default App;
